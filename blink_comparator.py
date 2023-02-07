f01dcd1dd29b6af474ecbb5d725d92e4632744d9
Michal <innewiadro@gmail.com>
2023-02-07 21:34:03 +0100

    -add funcs find_best_matches, qc_best_matches, register_image

--- blink_comparator.py
+++ blink_comparator.py
@@ -6,19 +6,39 @@ import cv2 as cv
 MIN_NUM_KEYPOINT_MATCHES = 50
 
 
-def find_best_matches():
+def find_best_matches(img1, img2):
     """returns a list of key points and a list of best matches"""
-    pass
+    orb = cv.ORB_create(nfeatures=100)  # create object ORB
+    kp1, desc1 = orb.detectAndCompute(img1, mask=None)
+    kp2, desc2 = orb.detectAndCompute(img2, mask=None)
+    bf = cv.BFMatcher(cv.NORM_HAMMING, crossChecker=True)
+    matches = bf.match(desc1, desc2)
+    matches = sorted(matches, key=lambda x: x.distance)
+    best_matches = matches[:MIN_NUM_KEYPOINT_MATCHES]
+    return kp1, kp2, best_matches
 
 
-def qc_best_matches():
+def qc_best_matches(img_match):
     """draw matches of points connected by lines"""
-    pass
+    cv.imshow(f"{MIN_NUM_KEYPOINT_MATCHES} najlepiej dopasowane punkty kluczowe", img_match)  # TODO english
+    cv.waitKey(3000)
 
 
-def register_image():
+def register_image(img1, img2, kp1, kp2, best_matches):
     """returns the first image recorded to the second"""
-    pass
+    if len(best_matches) >= MIN_NUM_KEYPOINT_MATCHES:
+        src_pts = np.zeros((len(best_matches), 2), dtype=np.float32)
+        dst_pts = np.zeros((len(best_matches), 2), dtype=np.float32)
+        for i, match in enumerate(best_matches):
+            src_pts[i, :] = kp1[match.queryIdx].pt
+            dst_pts[i, :] = kp2[match.trainIdx].pt
+        h_array, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC)
+        height, width = img2.shape
+        img1_wraped = cv.wrapPerspective(img1, h_array, (width, height))
+        return img1_wraped
+    else:
+        print(f"WARNING: Number of key points less than {MIN_NUM_KEYPOINT_MATCHES}\n")  # TODO eglish
+        return img1
 
 
 def blink():
@@ -45,7 +65,7 @@ def main():
 
         height, width = img1.shape
         cv.line(img_match, (width, 0), (width, height), (255, 255, 255), 1)
-        # qc_best_matches(img_match)
+        qc_best_matches(img_match)
         img1_registred = register_image(img1, img2, kp1, kp2, best_matches)
 
         blink(img1, img1_registred, "verification of registration", num_loops=5)
